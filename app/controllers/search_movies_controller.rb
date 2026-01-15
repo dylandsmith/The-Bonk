874a9be1563815815da88f332487f2c0e86baa59
Dylan Smith <dylansmith@Dylans-MacBook-Pro.local>
2026-01-14 20:45:16 -0800

    WIP

--- app/controllers/search_movies_controller.rb
+++ app/controllers/search_movies_controller.rb
@@ -1,13 +1,16 @@
 class SearchMoviesController < ApplicationController
   def index
-    @movies =
-      if params[:query].present?
-        Favorite
-          .where(favoritable_type: "Movie")
-          .joins("INNER JOIN movies ON movies.id = favorites.favoritable_id")
-          .where("LOWER(movies.title) LIKE LOWER(?)", "%#{params[:query]}%")
-      else
-        Favorite.none
-      end
+    keyword = "%#{params[:query]}%"
+    @movies = Movie.where("title ILIKE ?", keyword)
+    # @movies =
+    #   if params[:query].present?
+    #     Favorite
+    #       .where(favoritable_type: "Movie")
+    #       .joins("INNER JOIN movies ON movies.id = favorites.favoritable_id")
+    #       .where("LOWER(movies.title) LIKE LOWER(?)", "%#{params[:query]}%")
+    #   else
+    #     Favorite.none
+    #   end
+    # binding.pry
   end
 end
